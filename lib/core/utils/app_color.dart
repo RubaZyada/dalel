e5330a5bc0060294a4425c1a7368a0ebd3fc671c
RubaZyada <robazyada167@gmail.com>
2026-03-11 00:43:11 +0200

    app color

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+abstract class  AppColor{
+  static  Color primaryColor = const Color(0xffC49E85);
+  static  Color offWhiteColor = const Color(0xffFF8F4F9);
+  static  Color deebBrown = const Color(0xff6B4B3E);
+  static  Color deebGrey = const Color(0xff6F6460);
+  static  Color lightGrey = const Color(0xffEDEDED);
+  static  Color blackColor = const Color(0xff333333);
+
+}
