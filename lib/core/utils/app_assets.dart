c65bf9af557fa323f35026ec135bd795daf3c99c
RubaZyada <robazyada167@gmail.com>
2026-03-11 00:33:37 +0200

    flutter assets gen

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  //private constructor to prevent instantiation
+  Assets._();
+  
+  /// Assets for Onboarding1
+  /// assets/images/onboarding1.png
+  static const String Onboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for Onboarding2
+  /// assets/images/onboarding2.png
+  static const String Onboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for Onboarding3
+  /// assets/images/onboarding3.png
+  static const String Onboarding3 = "assets/images/onboarding3.png";
+
+  /// Assets for PassIcon
+  /// assets/images/pass_icon.png
+  static const String PassIcon = "assets/images/pass_icon.png";
+
+  /// Assets for Succces order icon
+  /// assets/images/Succces order icon.png
+  static const String Succces_order_icon = "assets/images/Succces order icon.png";
+
+  /// Assets for Svg1
+  /// assets/images/svg1.svg
+  static const String Svg1 = "assets/images/svg1.svg";
+
+  /// Assets for Svg2
+  /// assets/images/svg2.svg
+  static const String Svg2 = "assets/images/svg2.svg";
+}
+
