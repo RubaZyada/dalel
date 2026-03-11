8c9ab48b103a0bffa9336ed7463a91c537507ca2
RubaZyada <robazyada167@gmail.com>
2026-03-11 14:32:15 +0200

    app String

---
+++ lib/core/utils/app_string.dart
@@ -0,0 +1,34 @@
+abstract class AppStrings {
+  // App
+  static const String appName = "Dalel";
+  //onboarding
+  static const String onboardingTitle1 =
+      "Using modern AI technology for better user experience";
+  static const String onboardingTitle2 =
+      "Save your favorite recipes and never lose them again.";
+  static const String onboardingTitle3 =
+      "Share your culinary creations with the world and inspire others.";
+  static const String onboardingDesc1 =
+      "Using our app’s history libraries you can find many historical periods ";
+  static const String onboardingDesc2 =
+      "A big variety of ancient places from all over the world";
+  static const String onboardingDesc3 =
+      "AI provide recommendations and helps you to continue the search journey";
+  static const String skip = "Skip";
+  static const String next = "Next";
+  // Auth
+  static const String createAccont = "Create Account";
+  static const String signUp = "Sign Up";
+  static const String signIn = "Sign In";
+
+  // Buttons
+  static const String checkOut = "CheckOut Now";
+  static const String confirmPayment = "Confirm Payment";
+  static const String back = "Back To Home";
+  static const String sendVerification = "Send Verification Code";
+  static const String verify = "Verification Now";
+
+  // Errors
+  static const String noInternet = "No Internet Connection";
+  static const String somethingWrong = "Something went wrong";
+}
